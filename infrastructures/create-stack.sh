8ed3b07c7f8ff907ca93cf748c054b68d6c4af48
Michael Olatunji <tundexmike@gmail.com>
2020-05-28 14:51:54 +0100

    modified nodes.yml, changed EKS version

--- infrastructures/create-stack.sh
+++ infrastructures/create-stack.sh
@@ -2,5 +2,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---capababilities CAPABILITY_IAM \
+--capabilities CAPABILITY_IAM \
 --region=us-west-2
--- infrastructures/update-stack.sh
+++ infrastructures/update-stack.sh
@@ -1,6 +1,6 @@
-aws cloudformation create-stack \
+aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---capababilities CAPABILITY_IAM \
+--capabilities CAPABILITY_IAM \
 --region=us-west-2
